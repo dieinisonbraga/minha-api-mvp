99f24ca2adc29c210b6e1e67f612eb9e0db94a72
Dieinison Jack Freire Braga <dieinisonbraga@gmail.com>
2024-12-11 19:49:18 -0300

    Delete tira-duvidas-04 directory

--- tira-duvidas-04/tira-duvidas-04/src/components/videosConst.js
+++
@@ -1,7 +0,0 @@
-import agachamentoGif from '../assets/agachamento.gif'
-import alongamentoGif from '../assets/alongamento.gif'
-
-export const VIDEOS_TYPES = {
-    agachamento: agachamentoGif,
-    alongamento: alongamentoGif
-}   
