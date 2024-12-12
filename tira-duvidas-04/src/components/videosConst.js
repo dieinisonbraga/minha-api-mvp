88994569af62f2f11b44b19ce0f8767252e78433
Dieinison Jack Freire Braga <dieinisonbraga@gmail.com>
2024-12-12 19:41:12 -0300

    Adicionando uma pasta via upload

---
+++ tira-duvidas-04/src/components/videosConst.js
@@ -0,0 +1,7 @@
+import agachamentoGif from '../assets/agachamento.gif'
+import alongamentoGif from '../assets/alongamento.gif'
+
+export const VIDEOS_TYPES = {
+    agachamento: agachamentoGif,
+    alongamento: alongamentoGif
+}   
